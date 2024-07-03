d45bc1b8b0b1355e8322c32c5505f3f002cca0fb
Srijon Ashraf <72869452+srijonashraf@users.noreply.github.com>
2024-07-03 18:36:01 +0600

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <p className="text-indigo-700 text-3xl">Hello Bangladesh!</p>
+    </div>
+  );
+};
+
+export default App;
