47f0104e8570dc55f7f98ee938888a0199f3b7d4
Srijon Ashraf <72869452+srijonashraf@users.noreply.github.com>
2024-07-04 23:59:40 +0600

    newsfeed path added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React, { Fragment } from "react";
 import Login from "./pages/Login";
 import Registration from "./pages/Registration";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NewsFeed from "./pages/NewsFeed";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
           <Route path="/login" element={<Login />} />
           <Route path="/registration" element={<Registration />} />
           <Route path="/*" element={<Login />} />
+          <Route path="/newsfeed" element={<NewsFeed />} />
         </Routes>
       </Router>
     </Fragment>
