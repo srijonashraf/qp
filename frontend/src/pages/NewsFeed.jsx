3dbd16472045853fbf75c44c9bb59a32235a6d96
Srijon Ashraf <72869452+srijonashraf@users.noreply.github.com>
2024-07-06 15:49:50 +0600

    grid modified

--- frontend/src/pages/NewsFeed.jsx
+++ frontend/src/pages/NewsFeed.jsx
@@ -1,30 +1,23 @@
 import React from "react";
-import LeftMenu from "../components/newsfeed/leftSidebar/LeftMenu";
-import Suggestion from "./../components/newsfeed/main/suggestions/Suggestion";
+import LeftSidebar from "../components/newsfeed/leftSidebar/LeftSidebar.jsx";
+import Suggestion from "../components/newsfeed/main/suggestions/Suggestion";
 import Posts from "./../components/newsfeed/main/posts/Posts";
-import RightMenu from "../components/newsfeed/rightSidebar/RightMenu";
+import RightSidebar from "../components/newsfeed/rightSidebar/RightSidebar.jsx";
 
 const NewsFeed = () => {
   return (
-    <div className="bg-body-base-color/20 h-screen grid grid-cols-12 pt-2 gap-2">
-      {/* Left Menu: Visible from md breakpoint */}
-      <div className="hidden md:block md:col-span-3 lg:col-span-2 sm:col-0 bg-white rounded hover:overflow-y-auto">
-        <LeftMenu />
+    <div className="bg-body-base-color/20 grid grid-cols-12 gap-2 min-h-screen">
+      <div className="hidden md:block md:col-span-3 lg:col-span-2 sticky top-0">
+        <LeftSidebar />
       </div>
-      {/* Main Content: Adjusting columns based on screen size */}
-      <div className="grid grid-cols-12 gap-2 md:col-span-9 lg:col-span-10 col-span-12">
-        {/* Suggestions: Visible from lg breakpoint */}
-        <div className="hidden lg:block lg:col-span-3 bg-white rounded">
-          <Suggestion />
-        </div>
-        {/* Posts: Full width on small screens */}
-        <div className="col-span-12 lg:col-span-6 md:col-span-12 bg-white rounded">
-          <Posts />
-        </div>
-        {/* Right Menu: Visible from lg breakpoint */}
-        <div className="hidden lg:block lg:col-span-3 bg-white rounded">
-          <RightMenu />
-        </div>
+      <div className="hidden lg:block lg:col-span-2">
+        <Suggestion />
+      </div>
+      <div className="lg:col-span-6 md:col-span-9 col-span-12">
+        <Posts />
+      </div>
+      <div className="hidden lg:block lg:col-span-2">
+        <RightSidebar />
       </div>
     </div>
   );
