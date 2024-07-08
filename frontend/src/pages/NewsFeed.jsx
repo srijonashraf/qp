9f029393df5892e9d5ab9d2bbbe0983022aee1e8
Srijon Ashraf <72869452+srijonashraf@users.noreply.github.com>
2024-07-08 17:44:37 +0600

    ui enhanced

--- frontend/src/pages/NewsFeed.jsx
+++ frontend/src/pages/NewsFeed.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import LeftSidebar from "../components/newsfeed/leftSidebar/LeftSidebar.jsx";
 import Suggestion from "../components/newsfeed/main/suggestions/Suggestion";
-import Posts from "./../components/newsfeed/main/posts/Posts";
+import Timeline from "../components/newsfeed/main/timeline/Timeline.jsx";
 import RightSidebar from "../components/newsfeed/rightSidebar/RightSidebar.jsx";
 
 const NewsFeed = () => {
@@ -14,7 +14,7 @@ const NewsFeed = () => {
         <Suggestion />
       </div>
       <div className="lg:col-span-6 md:col-span-9 col-span-12">
-        <Posts />
+        <Timeline />
       </div>
       <div className="hidden lg:block lg:col-span-2">
         <RightSidebar />
