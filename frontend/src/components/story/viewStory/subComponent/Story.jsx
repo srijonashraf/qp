ebf1af37ac83682d413a6ec4ce0790d6c2018a50
Srijon Ashraf <72869452+srijonashraf@users.noreply.github.com>
2024-07-12 12:27:36 +0600

    story reaction and reply added

--- frontend/src/components/story/viewStory/subComponent/Story.jsx
+++ frontend/src/components/story/viewStory/subComponent/Story.jsx
@@ -4,6 +4,12 @@ import RightArrowIcon from "../../../../assets/icons/newsfeed/RightArrowIcon";
 import LeftArrowIcon from "../../../../assets/icons/newsfeed/LeftArrowIcon";
 import "./story.css";
 import { useNavigate } from "react-router-dom";
+import HeartIcon from "../../../../assets/icons/newsfeed/HeartIcon";
+import AngryIcon from "../../../../assets/icons/newsfeed/AngryIcon";
+import WowIcon from "../../../../assets/icons/newsfeed/WowIcon";
+import LikeBlueIcon from "../../../../assets/icons/newsfeed/LikeBlueIcon";
+import HahaIcon from "../../../../assets/icons/newsfeed/HahaIcon";
+import SadIcon from "../../../../assets/icons/newsfeed/SadIcon";
 
 const Story = () => {
   const navigate = useNavigate();
@@ -33,6 +39,23 @@ const Story = () => {
         <LeftArrowIcon className="text-gray-800 z-50 absolute left-4 top-1/2 transform -translate-y-1/2 cursor-pointer" />
         <RightArrowIcon className="text-gray-800 z-50 absolute right-4 top-1/2 transform -translate-y-1/2 cursor-pointer" />
         <div className="absolute top-0 left-0 w-0 h-1 bg-brand-primary animate-slider"></div>
+
+        <div className="px-2 overflow-x-auto flex absolute bottom-2 items-center w-full">
+          <input
+            type="text"
+            placeholder="Reply"
+            className=" bg-body-base-color/50 h-10 p-3 rounded-lg
+              placeholder:text-white w-full"
+          />
+          <div className="flex items-center">
+            <LikeBlueIcon />
+            <HeartIcon />
+            <HahaIcon />
+            <WowIcon />
+            <SadIcon />
+            <AngryIcon />
+          </div>
+        </div>
         <img src={userPortrait} alt="" className="w-full h-full object-cover" />
       </div>
     </div>
